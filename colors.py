15ea409c6075e946798f393227681e5ad9ac7630
Regedit-msc <efusanyaae@gmail.com>
2021-12-01 15:07:29 +0100

    removed portion

--- colors.py
+++ colors.py
@@ -26,9 +26,9 @@ venn2([rainbow_colors, primary_colors],
 venn2_circles([rainbow_colors,
               primary_colors])
 plt.show()
-intList = rainbow_colors.intersection(
-    primary_colors)
-A = intVal.openclosed(10, 25)
-B = intVal.openclosed(20, 50)
-C = A.union(B)
-D = B.intersection(A)
+# intList = rainbow_colors.intersection(
+#     primary_colors)
+# A = intVal.openclosed(10, 25)
+# B = intVal.openclosed(20, 50)
+# C = A.union(B)
+# D = B.intersection(A)
